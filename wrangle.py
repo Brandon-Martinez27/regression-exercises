efdc5bf0460e95dd27094ab704cf39cfc7b364b7
“Brandon <Brandon.joseph.martinez3@gmail.com>
2020-09-29 12:32:45 -0500

    start explore lession (codealong), start explore exercises, update wrangle file for explore lesson

--- wrangle.py
+++ wrangle.py
@@ -56,3 +56,14 @@ def wrangle_telco():
     df.drop(columns=['customer_id'], inplace=True)
     
     return df
+
+#################### Wrangle Grades Data ##################
+
+def wrangle_grades():
+    grades = pd.read_csv("student_grades.csv")
+    grades.drop(columns="student_id", inplace=True)
+    grades.replace(r"^\s*$", np.nan, regex=True, inplace=True)
+    df = grades.dropna().astype("int")
+    return df
+
+
